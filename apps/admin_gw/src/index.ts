b976f14e37c77db4bd35a9e80bebedafdb8dbfaa
Viktor Pyatakov <pyatakov.viktor@gmail.com>
2022-06-19 22:32:10 +0300

    admin

--- apps/admin_gw/src/index.ts
+++ apps/admin_gw/src/index.ts
@@ -14,11 +14,11 @@ import protectedRoutes from './config/routes/protected';
 
 
     app.addRouter(new Router(publicRoutes));
-    // app.addRouter(new JwtProxy(new Router(protectedRoutes), {
-    //   cookieName: process.env['COOKIE_NAME'],
-    //   uriCheck: process.env['IDENTITY_API_SRV'] + '/.well-known',
-    //   uriRefresh: process.env['IDENTITY_API_SRV'] + '/token/refresh',
-    // }));
+    app.addRouter(new JwtProxy(new Router(protectedRoutes), {
+      cookieName: process.env['COOKIE_NAME'],
+      uriCheck: process.env['IDENTITY_API_SRV'] + '/.well-known',
+      uriRefresh: process.env['IDENTITY_API_SRV'] + '/token/refresh',
+    }));
 
     await app.start();
   }
